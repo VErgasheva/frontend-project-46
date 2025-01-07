50cdb078b07287f0320d6745edfd2392189ed5a0
VErgasheva <vikser587@gmail.ru>
2025-01-07 11:31:30 +0500

    2

--- gendiff.js
+++ gendiff.js
@@ -6,7 +6,26 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0')
+  .version('1.0.0', '-V, --version', 'output the version number')
   .helpOption('-h, --help', 'output usage information');
 
 program.parse(process.argv);
+
+program
+  .option('-f, --format [type]', 'output format');
+
+program
+  .arguments('<filepath1> <filepath2>')
+  .description('Two file paths to compare')
+  .action((filepath1, filepath2) => {
+    console.log(`Comparing files: ${filepath1} and ${filepath2}`);
+  });
+
+program
+  .helpOption('-h, --help', 'output usage information');
+
+program.parse(process.argv);
+
+if (program.args.length < 2) {
+  program.help();
+}
