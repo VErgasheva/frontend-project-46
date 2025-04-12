e2b7ab73920ca6b50b5e69be12769a45fc2065d7
VErgasheva <vikser587@gmail.ru>
2025-04-12 15:14:55 +0500

    3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 
+import path from 'node:path'; 
+import { parseFile } from '../index.js';
 import { Command } from 'commander';
+import parseFile from './src/parser.js';
 
 const program = new Command();
 
@@ -8,15 +11,18 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0') 
-  .argument('<filepath1>', 'path to the first file')
-  .argument('<filepath2>', 'path to the second file')
+  .arguments('<filepath1> <filepath2>')
   .option('-f, --format [type]', 'output format')
   .helpOption('-h, --help', 'display help for command')
-  .action((filepath1, filepath2, options) => {
-    console.log(`Comparing files: ${filepath1} and ${filepath2}`);
-    if (options.format) {
-      console.log(`Output format: ${options.format}`);
-    }
+  .action((filepath1, filepath2) => {
+    const absolutePath1 = path.resolve(process.cwd(), filepath1);
+    const absolutePath2 = path.resolve(process.cwd(), filepath2);
+
+    const data1 = parseFile(absolutePath1);
+    const data2 = parseFile(absolutePath2);
+
+    console.log('File 1:', data1);
+    console.log('File 2:', data2);
   });
 
 program.parse();
