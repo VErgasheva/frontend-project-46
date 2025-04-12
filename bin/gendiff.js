3326df995b2a920e82f0c9e9d757d8be08bd5329
VErgasheva <vikser587@gmail.ru>
2025-04-12 14:34:58 +0500

    2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,7 +7,16 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0')
-  .helpOption('-h, --help', 'display help for command');
+  .version('1.0.0') 
+  .argument('<filepath1>', 'path to the first file')
+  .argument('<filepath2>', 'path to the second file')
+  .option('-f, --format [type]', 'output format')
+  .helpOption('-h, --help', 'display help for command')
+  .action((filepath1, filepath2, options) => {
+    console.log(`Comparing files: ${filepath1} and ${filepath2}`);
+    if (options.format) {
+      console.log(`Output format: ${options.format}`);
+    }
+  });
 
 program.parse();
