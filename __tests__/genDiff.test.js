ead5b95c12b3f93c927c9c99e5a3a3eaa77ef1a1
VErgasheva <vikser587@gmail.ru>
2025-04-29 20:32:40 +0500

    6

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,31 +1,35 @@
 const fs = require('fs');
 const path = require('path');
-const { genDiff } = require('../index.js');
+const genDiff = require('../src/index.js');  
 const { parseContent } = require('../src/parsers.js');
-const { file11Fixture } = require('../__fixtures__/objs.js');
+const { file11Fixture } = require('../fixtures/objs.js');
 
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures', filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 test.each([
   'stylish',
   'plain',
   'json',
 ])('genDiff %s', (format) => {
-  expect(genDiff(
-    getFixturePath('file11.json'),
-    getFixturePath('file22.json'),
-    format,
-  )).toEqual(fs.readFileSync(getFixturePath(`result_${format}.txt`), 'utf-8'));
+  expect(
+    genDiff(
+      getFixturePath('file11.json'),
+      getFixturePath('file22.json'),
+      format,
+    )
+  ).toEqual(
+    fs.readFileSync(getFixturePath(`result_${format}.txt`), 'utf-8')
+  );
 });
 
 test('parseContent yaml', () => {
   expect(
-    parseContent(fs.readFileSync(getFixturePath('file11.yml'), 'utf-8')),
+    parseContent(fs.readFileSync(getFixturePath('file11.yml'), 'utf-8'))
   ).toEqual(file11Fixture);
 });
 
 test('parseContent json', () => {
   expect(
-    parseContent(fs.readFileSync(getFixturePath('file11.json'), 'utf-8')),
+    parseContent(fs.readFileSync(getFixturePath('file11.json'), 'utf-8'))
   ).toEqual(file11Fixture);
 });
