87317df2b41cf3de33e6f84721de217a775ed9b9
VErgasheva <vikser587@gmail.ru>
2025-04-30 19:56:47 +0500

    6

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-import { parseFile } from '../src/parsers.js';
+import { parseContent } from '../src/parsers.js';
 import genDiff from '../src/genDiff.js';
 
 const program = new Command();
@@ -13,8 +13,8 @@ program
   .option('-f, --format [type]', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
-    const obj1 = parseFile(filepath1);
-    const obj2 = parseFile(filepath2);
+    const obj1 = parseContent(filepath1);
+    const obj2 = parseContent(filepath2);
     const diff = genDiff(obj1, obj2);
     console.log(diff);
   });
